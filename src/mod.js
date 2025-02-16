d5f22bc86157cd547d9a9a7decd3b593370baa0b
MoxoPixel <hello@emils.graphics>
2025-02-16 12:34:13 +0100

    Updated to 1.0.7

--- src/mod.js
+++ src/mod.js
@@ -1,19 +1,33 @@
-import { DependencyContainer } from "tsyringe";
-import { ILogger } from "@spt/models/spt/utils/ILogger";
-import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
-import { IPostDBLoadMod } from "@spt/models/external/IPostDBLoadMod";
-import { DatabaseServer } from "@spt/servers/DatabaseServer";
-import { ConfigServer } from "@spt/servers/ConfigServer";
-import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
-import { BaseClasses } from "@spt/models/enums/BaseClasses";
-import { ILostOnDeathConfig } from "@spt/models/spt/config/ILostOnDeathConfig";
-import { IBotConfig } from "@spt/models/spt/config/IBotConfig";
-
-import * as configData from "../config.json";
-
-class TMS implements IPreSptLoadMod, IPostDBLoadMod {
-
-    private readonly loadingMessages = [
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ConfigTypes_1 = require("C:/snapshot/project/obj/models/enums/ConfigTypes");
+const BaseClasses_1 = require("C:/snapshot/project/obj/models/enums/BaseClasses");
+const configData = __importStar(require("../config.json"));
+class TMS {
+    loadingMessages = [
         "Time to bend Tarkovs rules harder than a scav bends to loot",
         "Reshaping Tarkov like a chad reshapes his meta M4",
         "Modifying Tarkov faster than Mechanic modifies guns",
@@ -24,32 +38,25 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
         "Adjusting settings faster than a chad adjusts his sights",
         "Tweaking Tarkov harder than a PMC tweaks his gear"
     ];
-
-    private getRandomLoadingMessage(): string {
+    getRandomLoadingMessage() {
         return this.loadingMessages[Math.floor(Math.random() * this.loadingMessages.length)];
     }
-
-    public configServer: ConfigServer;
-    public container: DependencyContainer;
-
+    configServer;
+    container;
     constructor() { }
-
-    public preSptLoad(container: DependencyContainer): void {
+    preSptLoad(container) {
         this.container = container;
-        this.configServer = container.resolve<ConfigServer>("ConfigServer");
+        this.configServer = container.resolve("ConfigServer");
     }
-
-    public postDBLoad(container: DependencyContainer): void {
-        const logger = container.resolve<ILogger>("WinstonLogger");
-
-        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+    postDBLoad(container) {
+        const logger = container.resolve("WinstonLogger");
+        const databaseServer = container.resolve("DatabaseServer");
         const globals = databaseServer.getTables().globals.config;
         const tables = databaseServer.getTables();
         const dbItems = tables.templates.items;
         const enemyTypes = tables.bots.types;
-
         if (configData.EnableLostOnDeath) {
-            const dbLODConfig = this.configServer.getConfig<ILostOnDeathConfig>(ConfigTypes.LOST_ON_DEATH);
+            const dbLODConfig = this.configServer.getConfig(ConfigTypes_1.ConfigTypes.LOST_ON_DEATH);
             dbLODConfig.questItems = configData.LostOnDeath.QuestItems;
             dbLODConfig.specialSlotItems = configData.LostOnDeath.SpecialSlotItems;
             dbLODConfig.equipment.ArmBand = configData.LostOnDeath.ArmBand;
@@ -66,7 +73,6 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
             dbLODConfig.equipment.SecondPrimaryWeapon = configData.LostOnDeath.SecondPrimaryWeapon;
             dbLODConfig.equipment.PocketItems = configData.LostOnDeath.PocketItems;
         }
-
         if (configData.FUInertia) {
             globals.Inertia.BaseJumpPenalty = 0.03;
             globals.Inertia.ExitMovementStateSpeedThreshold.x = 0.001;
@@ -83,22 +89,18 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
             globals.Inertia.WalkInertia.x = 0.002;
             globals.Inertia.WalkInertia.y = 0.025;
         }
-
         if (configData.HigherVaulting) {
             globals.VaultingSettings.MovesSettings.ClimbSettings.MoveRestrictions.MaxLength = 17;
             globals.VaultingSettings.MovesSettings.ClimbSettings.MoveRestrictions.MaxHeight = 2.2;
         }
-
         if (configData.EnableCustomBotsHealth) {
             const enemyTypes = tables.bots.types;
-
             const bossHealthMultiplierValue = configData.bossHealthMultiplier;
             const followerHealthMultiplierValue = configData.followerHealthMultiplier;
             const specialHealthMultiplierValue = configData.specialHealthMultiplier;
             const pmcHealthMultiplierValue = configData.pmcHealthMultiplier;
             const scavHealthMultiplierValue = configData.scavHealthMultiplier;
             const infectedHealthMultiplierValue = configData.infectedHealthMultiplier;
-
             for (const key in enemyTypes) {
                 if (key.startsWith('boss')) {
                     const bossBot = enemyTypes[key];
@@ -168,11 +170,10 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
                 }
             }
         }
-
         if (configData.MakeAllHeadsetsGood) {
             const headsetValues = configData.HeadsetValues;
             for (const item in dbItems) {
-                if (dbItems[item]._parent === BaseClasses.HEADPHONES) {
+                if (dbItems[item]._parent === BaseClasses_1.BaseClasses.HEADPHONES) {
                     const itemProps = dbItems[item]._props;
                     itemProps.Distortion = headsetValues.Distortion;
                     itemProps.CompressorTreshold = headsetValues.CompressorTreshold;
@@ -207,67 +208,62 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
                 }
             }
         }
-
         if (configData.MakeRepairKitLessOverpowered) {
             const ArmorRepairKit = "591094e086f7747caa7bb2ef";
             const ArmorRepair = dbItems[ArmorRepairKit];
             ArmorRepair._props.MaxRepairResource = configData.ArmorRepairKitMaxResource;
-
             const WeaponRepairKit = "5910968f86f77425cf569c32";
             const WeaponRepair = dbItems[WeaponRepairKit];
             WeaponRepair._props.MaxRepairResource = configData.WeaponRepairKitMaxResource;
         }
-
         if (configData.ColouredAmmo) {
             for (const item in dbItems) {
-                if (dbItems[item]._parent === BaseClasses.AMMO) {
+                if (dbItems[item]._parent === BaseClasses_1.BaseClasses.AMMO) {
                     const itemProps = dbItems[item]._props;
                     const pen = itemProps.PenetrationPower;
                     let colour = '';
-
                     if (pen > 60) {
                         colour = 'red'; // SuperPen
-                    } else if (pen > 50) {
+                    }
+                    else if (pen > 50) {
                         colour = 'yellow'; // HighPen
-                    } else if (pen > 40) {
+                    }
+                    else if (pen > 40) {
                         colour = 'violet'; // MedHighPen
-                    } else if (pen > 30) {
+                    }
+                    else if (pen > 30) {
                         colour = 'blue'; // MedPen
-                    } else if (pen > 20) {
+                    }
+                    else if (pen > 20) {
                         colour = 'green'; // LowMedPen
-                    } else {
+                    }
+                    else {
                         colour = 'grey'; // LowPen
                     }
-
                     itemProps.BackgroundColor = colour;
-
                     if (configData.WeightlessAmmo) {
                         itemProps.Weight = 0;
                     }
                 }
             }
         }
-
         if (configData.DisableMagazineAmmoLoadPenalty) {
             for (const item in dbItems) {
-                if (dbItems[item]._parent === BaseClasses.MAGAZINE) {
+                if (dbItems[item]._parent === BaseClasses_1.BaseClasses.MAGAZINE) {
                     const itemProps = dbItems[item]._props;
                     itemProps.LoadUnloadModifier = 0;
                 }
             }
         }
-
         if (configData.AdjustBotGrenades) {
             const botGrenadePrecisionValue = Math.max(0, configData.BotGrenadePrecision);
             const botGrenadePerMeterValue = Math.max(0, configData.BotGrenadePerMeter);
-
             try {
                 for (const key in enemyTypes) {
-                    if (!enemyTypes.hasOwnProperty(key)) continue;
-
+                    if (!enemyTypes.hasOwnProperty(key))
+                        continue;
                     const bot = enemyTypes[key];
                     const difficulties = bot?.difficulty;
-
                     if (difficulties) {
                         ['easy', 'normal', 'hard', 'impossible'].forEach(level => {
                             if (difficulties[level]?.Grenade) {
@@ -277,35 +273,34 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
                         });
                     }
                 }
-            } catch (error) {
+            }
+            catch (error) {
                 logger.error(`Error adjusting bot grenades: ${error.message}`);
             }
         }
-
         if (configData.MakeForegripsEqual) {
             const ForegripsErgo = configData.ForegripsErgo;
             const ForegripsRecoil = configData.ForegripsRecoil;
-
             try {
                 for (const item in dbItems) {
-                    if (!dbItems.hasOwnProperty(item)) continue;
-
+                    if (!dbItems.hasOwnProperty(item))
+                        continue;
                     const currentItem = dbItems[item];
-                    if (!currentItem?._props) continue;
-
-                    if (currentItem._parent === BaseClasses.FOREGRIP) {
+                    if (!currentItem?._props)
+                        continue;
+                    if (currentItem._parent === BaseClasses_1.BaseClasses.FOREGRIP) {
                         currentItem._props.Ergonomics = ForegripsErgo;
                         currentItem._props.Recoil = ForegripsRecoil;
                     }
                 }
-            } catch (error) {
+            }
+            catch (error) {
                 logger.error(`Error adjusting foregrips: ${error.message}`);
             }
         }
-
         if (configData.FixExtraSize) {
             for (const item in dbItems) {
-                if (dbItems[item]._parent === BaseClasses.MAGAZINE) {
+                if (dbItems[item]._parent === BaseClasses_1.BaseClasses.MAGAZINE) {
                     const itemProps = dbItems[item]._props;
                     if (dbItems[item]._id == "6513f0a194c72326990a3868") {
                         itemProps.ExtraSizeDown = 0;
@@ -322,7 +317,7 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
                         }
                     }
                 }
-                if (dbItems[item]._parent === BaseClasses.MOUNT) {
+                if (dbItems[item]._parent === BaseClasses_1.BaseClasses.MOUNT) {
                     const itemProps = dbItems[item]._props;
                     if (dbItems[item]._id == "5a37ca54c4a282000d72296a") {
                         itemProps.ExtraSizeUp = 0;
@@ -339,9 +334,8 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
                 }
             }
         }
-
         if (configData.EnableCustomBotCaps) {
-            const dbBotConfig = this.configServer.getConfig<IBotConfig>(ConfigTypes.BOT);
+            const dbBotConfig = this.configServer.getConfig(ConfigTypes_1.ConfigTypes.BOT);
             dbBotConfig.maxBotCap.factory4_day = configData.MaxBotCaps.factory4_day;
             dbBotConfig.maxBotCap.factory4_night = configData.MaxBotCaps.factory4_night;
             dbBotConfig.maxBotCap.bigmap = configData.MaxBotCaps.bigmap;
@@ -356,25 +350,21 @@ class TMS implements IPreSptLoadMod, IPostDBLoadMod {
             dbBotConfig.maxBotCap.sandbox_high = configData.MaxBotCaps.sandbox_high;
             dbBotConfig.maxBotCap.default = configData.MaxBotCaps.default;
         }
-
         if (configData.EnableMoreItemsSicc) {
             const SICC = tables.templates.items["5d235bb686f77443f4331278"];
             const filterArray = SICC["_props"]["Grids"][0]["_props"]["filters"][0]["Filter"];
-
             const newValuesToAdd = configData.AddTheseToPouch;
             filterArray.push(...newValuesToAdd);
         }
-
         if (configData.LegaMedalInsideMoneyCase) {
             const MONEYCASE = tables.templates.items["59fb016586f7746d0d4b423a"];
             const filterArray = MONEYCASE["_props"]["Grids"][0]["_props"]["filters"][0]["Filter"];
             filterArray.push("6656560053eaaa7a23349c86");
         }
-
         logger.info("---------------------------------------------------");
         logger.info(`TMS Loaded - ${this.getRandomLoadingMessage()}`);
         logger.info("---------------------------------------------------");
     }
 }
-
 module.exports = { mod: new TMS() };
+//# sourceMappingURL=mod.js.map
